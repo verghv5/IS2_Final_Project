f20d06e789ba62313e23c859c27c0a6aefd529e9
Vivek Verghese <30439889+verghv5@users.noreply.github.com>
2020-05-01 17:09:49 -0400

    Added Program Files

---
+++ IS2_Final_Code.cpp
@@ -0,0 +1,148 @@
+/*
+	Sample code for controlling the various sensors for
+	the VR Bike Trainer.
+
+	Author: Vivek Verghese
+	Date: 04/30/2020
+	Class: Inventor's Studio 2
+*/
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+#include <time.h>
+#include "mtrand.h"
+#include <stdlib.h>
+#include <cassert>
+
+double currentSpeed = 0; // The current speed of the user
+double currentGradient = 0; // The gradient "sensor" in this example
+
+void adjustGradient(double adjustment)
+{
+	/*
+		This function sends the data to the motor. In this case, it only changes
+		a global variable.
+	*/
+	currentGradient += adjustment;
+}
+
+void readSpeed()
+{
+	/*
+		This function reads the data from the sensor and changes it in the program.
+		In this program, it just uses a random function to increase/decrease the speed
+		within a small boundary.
+	*/
+	static MTRand mtrand;
+	bool positive = mtrand.randInt(1);
+	double newSpeed = mtrand.rand(0.9);
+
+	if ((positive || newSpeed >= currentSpeed / 6) && currentSpeed < 18.0)
+		currentSpeed += newSpeed;
+	else
+		currentSpeed -= newSpeed;
+}
+
+double readGradient()
+{
+	/*
+		This reads the gradient sensor on the bike. In this case it just reads the variable
+		and returns it.
+	*/
+	return currentGradient;
+}
+
+int main(int argc, char** argv)
+{
+	if (argc != 5) // Ensure that the program is given the proper parameters on start.
+	{
+		std::cerr << "The proper usage is ./" << argv[0] << " map debugging_output gradient_output distance_output" << std::endl;
+		return 1;
+	}
+
+	std::string map = argv[1];
+	bool debuggingOutput = atoi(argv[2]);
+
+	std::ofstream gradientOutput(argv[3]);
+	std::ofstream distanceOutput(argv[4]);
+
+	std::string gradientDataFile = map + "_Gradient_Data.txt"; // Used to load the gradient data
+	std::string recommendedSpeedFile = map + "_Recommended_Speed.txt"; // Used to load the max speed through curves
+
+	std::ifstream inGradient(gradientDataFile);
+	std::ifstream inSpeed(recommendedSpeedFile);
+
+	double currentDistance = 0;
+
+	double totalDistance = 0;
+	double tmpDistance = 0;
+
+	double targetGradient = 0;
+	double recommendedSpeed = 0;
+
+	double nextGradientChange = 0;
+	double nextSpeedChange = 0;
+
+	double gradientAdjustment = 0;
+
+	inGradient >> totalDistance >> nextGradientChange >> targetGradient;
+	assert(nextGradientChange == 0 && targetGradient == 0.0);
+
+	inSpeed >> tmpDistance >> nextSpeedChange >> recommendedSpeed;
+	assert(nextSpeedChange == 0 && recommendedSpeed == 0.0);
+
+	assert(totalDistance == tmpDistance);
+
+	clock_t currentTime = clock();
+	clock_t outputTimer = clock(); // The program will output when the output time reads 
+
+	clock_t startTime = clock();
+
+	while (currentDistance < totalDistance)
+	{
+		if ( debuggingOutput && ((double)clock() - (double)outputTimer) / CLOCKS_PER_SEC >= 5.0)
+		{
+			outputTimer = clock();
+			double timeElapsed = ((double)outputTimer - (double)startTime) / CLOCKS_PER_SEC;
+			std::cout << "Time Elapsed: " << (int)timeElapsed / 3600 << "h:" << ((int)timeElapsed / 60) % 60
+						<< "m:" << (int)timeElapsed % 60 << "s" << std::endl;
+			std::cout << "Progress: " << currentDistance << "m / " << totalDistance << "m" << std::endl;
+			std::cout << "Average Speed: " << currentDistance / timeElapsed << "m/s or " << currentDistance / timeElapsed * 2.237 << "mi/hr" << std::endl;
+			std::cout << "Current Speed: " << currentSpeed << "m/s or " << currentSpeed * 2.237 << "mi/hr" << std::endl;
+			std::cout << "Current Gradient: " << readGradient() << std::endl << std::endl;
+		}
+		double n = 0.01;
+		while( ((double)clock() - (double)currentTime)/CLOCKS_PER_SEC < n );
+		currentTime = clock();
+
+		if (currentDistance >= nextGradientChange)
+		{
+			inGradient >> nextGradientChange >> targetGradient;
+			gradientAdjustment = ((targetGradient - readGradient()) * n) / ((nextGradientChange - currentDistance) / currentSpeed);
+		}
+		if (currentDistance >= nextSpeedChange)
+			inSpeed >> nextSpeedChange >> recommendedSpeed;
+
+		readSpeed();
+		currentDistance += (currentSpeed * n);
+
+		adjustGradient(gradientAdjustment);
+
+		gradientOutput << readGradient() << std::endl;
+		distanceOutput << currentDistance << std::endl;
+	}
+
+	clock_t endTime = clock();
+
+	double totalTime = ((double)endTime - (double)startTime) / CLOCKS_PER_SEC;
+	std::cout << "\tEnd of Workout!" << std::endl;
+	std::cout << "Time Elapsed: " << (int)totalTime / 3600 << "h:" << ((int)totalTime / 60) % 60
+				<< "m:" << (int)totalTime % 60 << "s" << std::endl;
+	std::cout << "Progress: " << currentDistance << "m / " << totalDistance << "m" << std::endl;
+	std::cout << "Average Speed: " << currentDistance / totalTime << "m/s or " << currentDistance / totalTime * 2.237 << "mi/hr" << std::endl;
+	std::cout << "Current Speed: " << currentSpeed << "m/s or " << currentSpeed * 2.237 << "mi/hr" << std::endl;
+	std::cout << "Current Gradient: " << readGradient() << std::endl << std::endl;
+	return 0;
+}
---
+++ README.txt
@@ -0,0 +1,69 @@
+IS2 Final Project README.
+
+Author: Vivek Verghese
+Date 04/30/2020
+
+
+This README covers the format of the input files.
+
+Map files are not handled in this code, but the "bike's" current readings will be stored as global variables in this case, and accessing them will be merely looking at the global variables.
+
+All gradient data files are denoted as [map]_Gradient_Data.txt, and in the example, [map] will always be Example. There will also be recommended speed files denotes as [map]_Recommended_Speed.txt which is meant to know what speeds are recommended to go through curves or on straights. The format will be like rally race instructions (more on that below).
+
+All files will reference data in metric. While the data can be converted based on preference on the display, the backend will only rely on metric.
+
+[map]_Gradient_Data.txt:
+
+	These files will give the data at each change on the course.
+
+	Most of the data comes in the form of racers who have ridden along the route with tracking equipment on their bikes. One example for the Tour De France is from Mikel Nieve's data here:
+	http://home.trainingpeaks.com/public/workout/MIM3PEJE55W3J6PJ26DBD35XXQ
+
+	For the example file, since I have not written any sort of scrapers to average several racer's data, the numbers will be all made up.
+
+	The the values are space separated as the program reads each value by space (new lines count as a space, but it makes it more readable to separate dependent information by line).
+
+	Length_of_Course
+	Distance_From_Start Grade
+	.
+	.
+	.
+	Length_of_Course 0.0
+	(nothing on last line, but there needs to be a blank new line here)
+
+
+[map]_Recommended_Speed.txt:
+
+	These will be based off of physics calculations of max speeds that can be achieved on the course with respect to going around curves.
+
+	The equation used to calculate this will be fairly simple, as this is more of a theoretical maximum rather than one that considers actual conditions (like rain, ice, wind, etc.). The equation is:
+
+	speed = sqrt( (r * g * (sin(theta) + u * cos(theta)) ) / (cos(theta) - u * sin(theta)) )
+
+	r is the radius of the curve
+	g is the gravitational acceleration (9.81)
+	theta is the angle of the bank
+	u is the frictional constant of the road (~0.7 for dry roads, ~0.4 for wet roads)
+
+	The the values are space separated as the program reads each value by space (new lines count as a space, but it makes it more readable to separate dependent information by line).
+
+	Length_of_Course
+	Distance_From_Start Grade
+	.
+	.
+	.
+	Length_of_Course 0.0
+	(nothing on last line, but there needs to be a blank new line here)
+
+
+Running the program:
+
+	The program takes 4 arguments:
+	 1.	The name of the map - in this case, the name is "Example"
+	 2.	Debugging output - 0 means that only the end stats are printed, 1 means that simulation data is printed every 5 seconds.
+	 3.	Gradient data output file - saves the current gradient reading every tick to this file
+	 4.	Distance data output file - saves the current distance reading every tick to this file
+
+	 When running the .exe from the command line, it should look like:
+
+	 ./t.exe Example 0 g_out.txt d_out.txt
